58b6bc7991087a88d18d38b7cdeb6dbef03c0305
wayou <liuwayong@gmail.com>
2014-10-17 22:28:10 +0800

    ripple menu

--- themes/freemind/source/js/main.js
+++ themes/freemind/source/js/main.js
@@ -1,21 +1,57 @@
 $(function() {
-    $("#toc").tocify({
-        context: '.mypage',
-        scrollHistory: true,
-        theme: 'bootstrap3',
-        selectors: 'h1,h2'
-    });
+    var $toc = $("#toc");
+    if ( !! $toc.length) {
+        $("#toc").tocify({
+            context: '.mypage',
+            scrollHistory: true,
+            theme: 'bootstrap3',
+            selectors: 'h1,h2'
+        });
 
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
+        //sticky the toc
+        var $window = $(window),
+            $stickyEl = $('#toc'),
+            elTop = $stickyEl.offset().top;
+
+        $window.scroll(function() {
+            $stickyEl.toggleClass('sticky-scroll', $window.scrollTop() > elTop - 110);
+        });
+    }
+
+
+    // highlight the menu
+    menuHighlight();
+
 });
 
-function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top) {
-        $('#toc').addClass('stick');
-    } else {
-        $('#toc').removeClass('stick');
-    }
+//this is adapted from http://css-tricks.com/moving-highlight/
+function menuHighlight() {
+    var originalBG = $(".nav li").css("background-color"),
+        x, y, xy, bgWebKit, bgMoz,
+        lightColor = "rgba(255,255,255,0.75)",
+        gradientSize = 50;
+
+    $('.nav li')
+        .mousemove(function(e) {
+            x = e.pageX - this.offsetLeft;
+            y = e.pageY - this.offsetTop;
+            xy = x + " " + y;
+
+            // bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", 100, from(rgba(255,255,255,0.8)), to(rgba(255,255,255,0.0))), " + originalBG;
+            bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", "+gradientSize+", from(rgba(44,108,222,0.8)), to(rgba(66,133,244,1.0))), " + originalBG;
+            bgMoz = "-moz-radial-gradient(" + x + "px " + y + "px 45deg, circle, " + lightColor + " 0%, " + originalBG + " " + gradientSize + "px)";
+
+            $(this)
+                .css({
+                    background: bgWebKit
+                })
+                .css({
+                    background: bgMoz
+                });
+
+        }).mouseleave(function() {
+            $(this).css({
+                background: originalBG
+            });
+        });
 }
