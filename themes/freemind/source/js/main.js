8f727d5a3d73637ce80c97c7bb001e549612b7c6
wayou <liuwayong@gmail.com>
2014-10-17 21:14:49 +0800

    optimize the nav btn

--- themes/freemind/source/js/main.js
+++ themes/freemind/source/js/main.js
@@ -1,11 +1,21 @@
 $(function() {
     $("#toc").tocify({
-        context:'.mypage',
-        scrollHistory:true,
-        theme:'bootstrap3',
-        selectors:'h1,h2'
+        context: '.mypage',
+        scrollHistory: true,
+        theme: 'bootstrap3',
+        selectors: 'h1,h2'
     });
-    $(window).on('scroll',function(){
-        // $('.tocify-item.list-group-item.active').closest('.tocify-subheader.list-group').prev().addClass('');
-    });
+
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
 });
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#toc').addClass('stick');
+    } else {
+        $('#toc').removeClass('stick');
+    }
+}
